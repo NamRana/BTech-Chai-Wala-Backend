3ed2a79865a3dc335a78e89bd78cfc46160bfefe
Naman Rana <91771208+NamRana@users.noreply.github.com>
2022-11-08 19:34:07 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-  const { connection } = await mongoose.connect("mongodb+srv://Abhay:abhay@cluster0.ifw5vyj.mongodb.net/Burger?retryWrites=true&w=majority");
+  const { connection } = await mongoose.connect(process.env.MONGODN_URI);
 
   console.log(`Database is connect with ${connection.host}`);
 };
